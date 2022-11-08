2cae5aedb487ae6eb60bc026f633a00635af4c7b
Seongtae <stae1102@gmail.com>
2022-11-08 16:23:22 +0900

    feat: Generate user resource files
    Created resource files
    - Controller, Module, Service
    
    Fixes: #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { AppService } from './app.service';
       }),
       inject: [ConfigService],
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
