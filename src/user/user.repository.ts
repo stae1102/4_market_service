f46c6bf5c029d6d8dfe4f0a0130d1be7195620f2
Seongtae <stae1102@gmail.com>
2022-11-08 20:36:46 +0900

    feat: Create User repository
    Fixes: #5

---
+++ src/user/user.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './schemas/user.schemas';
+
+@Injectable()
+export class UsersRepository {
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    return await this.userModel.create(createUserDto);
+  }
+}
