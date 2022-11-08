3b92a199a1ab51979e1bf3a526c12ecc3b146cb2
Seongtae <stae1102@gmail.com>
2022-11-08 20:31:29 +0900

    feat: Design user Collection
    Fixes: #5

---
+++ src/user/schemas/user.schemas.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
+export class User extends Document {
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ default: 'normal' })
+  userType: 'normal' | 'seller' | 'admin';
+
+  @Prop()
+  deletedAt: null | Date;
+}
+export const UserSchema = SchemaFactory.createForClass(User);
