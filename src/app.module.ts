235bee29c540aaa8656eafb4843b1ff13b16677b
Seongtae <stae1102@gmail.com>
2022-11-08 21:58:50 +0900

    feat: Generate auth resources
    Controller, Module, Service
    
    Fixes: #5

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './user/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './user/users.module';
       inject: [ConfigService],
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
