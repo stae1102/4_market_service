eb17696b86018a754fda48a647907a65ccdf2497
Seongtae <stae1102@gmail.com>
2022-11-09 23:10:54 +0900

    chore: Add OwnerId Column to sellers collection
    Fixes: #10

--- src/sellers/schemas/sellers.schema.ts
+++ src/sellers/schemas/sellers.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 @Schema({ timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' } })
 export class Sellers extends Document {
@@ -11,6 +11,9 @@ export class Sellers extends Document {
 
   @Prop()
   deletedAt: null | Date;
+
+  @Prop({ type: Types.ObjectId, ref: 'Users._id' })
+  OwnerId: Types.ObjectId;
 }
 
 export const SellersSchema = SchemaFactory.createForClass(Sellers);
