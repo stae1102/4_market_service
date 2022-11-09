3897ba5447dc0b28179b74788a807c260b2e970f
Seongtae <stae1102@gmail.com>
2022-11-09 21:51:21 +0900

    chore: Change user collection name to users
    Fixes: #10

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -2,25 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User } from './schemas/users.schemas';
+import { Users } from './schemas/users.schemas';
 
 @Injectable()
 export class UsersRepository {
   constructor(
-    @InjectModel(User.name) private readonly userModel: Model<User>,
+    @InjectModel(Users.name) private readonly userModel: Model<Users>,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create(createUserDto: CreateUserDto): Promise<Users> {
     return await this.userModel.create(createUserDto);
   }
 
-  async findByEmail(email: string): Promise<User> {
+  async findByEmail(email: string): Promise<Users> {
     return await this.userModel.findOne({
       email,
     });
   }
 
-  async findById(id: string): Promise<User> {
+  async findById(id: string): Promise<Users> {
     return await this.userModel.findById({ _id: id });
   }
 }
