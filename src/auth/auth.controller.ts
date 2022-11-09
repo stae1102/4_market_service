5d297be43328812bd743dfd101347793dff9f04c
Seongtae <stae1102@gmail.com>
2022-11-09 12:21:38 +0900

    feat: Generate auth controller
    Fixes: #5

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards } from '@nestjs/common';
+import { User } from '../common/decorators/user.decorator';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('signIn')
+  async login(@User() user) {
+    console.log('next');
+    return this.authService.signIn(user);
+  }
 }
