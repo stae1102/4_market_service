9622362af42be034a16d74f7456768e395384d2c
Seongtae <stae1102@gmail.com>
2022-11-09 23:11:19 +0900

    chore: Inject sellers repository
    Fixes: #10

--- src/sellers/sellers.module.ts
+++ src/sellers/sellers.module.ts
@@ -3,12 +3,13 @@ import { SellersService } from './sellers.service';
 import { SellersController } from './sellers.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Sellers, SellersSchema } from './schemas/sellers.schema';
+import { SellersRepository } from './sellers.repository';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Sellers.name, schema: SellersSchema }]),
   ],
   controllers: [SellersController],
-  providers: [SellersService],
+  providers: [SellersService, SellersRepository],
 })
 export class SellersModule {}
