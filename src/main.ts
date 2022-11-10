cef970bfc491e87e7d15650186c6a9df6ad81d35
Seongtae <stae1102@gmail.com>
2022-11-10 15:23:00 +0900

    feat: Apply winston to logger
    Resolves: #15

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
