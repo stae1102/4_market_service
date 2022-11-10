627a313671d35e4c7dec6a383ccaef7aead4da12
Seongtae <stae1102@gmail.com>
2022-11-10 16:14:41 +0900

    feat: Change logger style
    Resolves: #19

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import { AppModule } from './app.module';
-import { winstonLogger } from './utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    logger: winstonLogger,
-  });
+  const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(WINSTON_MODULE_NEST_PROVIDER));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
